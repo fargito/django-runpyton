bf4095d098e75fa449247b279948e8fc4ec1632f
fargito <fr.farge96@gmail.com>
2020-03-04 21:55:09 +0100

    create orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    reference = models.CharField(max_length=8)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    creation_date = models.DateField(auto_now=False, auto_now_add=False)
+    due_date = models.DateField(auto_now=False, auto_now_add=False)
+    customer_name = models.CharField(max_length=50)
+    customer_address = models.CharField(max_length=50)
+    customer_city = models.CharField(max_length=50)
+    customer_zip_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.reference
