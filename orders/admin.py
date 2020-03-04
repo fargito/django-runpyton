f22cc1d898f6162ab49fa8c1757af7f9bc919817
fargito <fr.farge96@gmail.com>
2020-03-04 21:58:57 +0100

    register orders model to admin interface

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "reference",
+        "amount",
+        "creation_date",
+        "due_date",
+        "customer_name",
+        "customer_address",
+        "customer_city",
+        "customer_zip_code",
+    )
+
+
+admin.site.register(Order, OrderAdmin)
